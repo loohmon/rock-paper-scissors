1376d0a87100d3472e7adc0c8e51e5c2133277cf
Lo Martins <eloisarfm@gmail.com>
2022-01-19 16:25:57 -0300

    Working properly. No rounds.

--- script.js
+++ script.js
@@ -1,55 +1,99 @@
-let weapons = ['rock', 'paper', 'scissors'];
+const playerScoreDisplay = document.querySelector('#player-score')
+const computerScoreDisplay = document.querySelector('#computer-score')
+const scoreDisplay = document.querySelector('#score-container')
+const resultDisplay = document.querySelector('#message')
+const resetButton = document.querySelector('#reset')
+const rockChoice = document.querySelector('#rock')
+const paperChoice = document.querySelector('#paper')
+const scissorsChoice = document.querySelector('#scissors')
+let playerScore = 0;
+let computerScore = 0;
 
-// define what the player's selection is
+// // define what the player's selection is
 
-function playerPlay() {
-    let playerSelection = prompt("Choose your weapon!");
-    return playerSelection;
-}
+// function playerPlay() {
+//     let playerChoice = prompt("Choose your weapon!");
+//     return playerChoice;
+// }
 
-// randomize the computer's play
+// // randomize the computer's play
 
-function computerPlay() {
-    let computerSelection = weapons[Math.floor(Math.random() * weapons.length)];
-    console.log("Computer picked: ", computerSelection);
-    return computerSelection;
-}
+// function computerPlay() {
+//     let computerSelection = weapons[Math.floor(Math.random() * weapons.length)];
+//     return computerSelection;
+// }
 
 // play one round of rock paper scissors
 
 
-    function playRound(playerSelection, computerSelection) {
-        if (playerSelection === "rock" && computerSelection == "scissors" || playerSelection === 'scissors' && computerSelection === "paper" || playerSelection === 'paper' && computerSelection === "rock") {
-            playerScore++;
-            return `You win! \n You: ${playerScore} \n Computer: ${computerScore}`
-        }
-        else if (playerSelection === "rock" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "rock") {
-            computerScore++;
-            return `You lose! \n You: ${playerScore} \n Computer: ${computerScore}`
-        }
-        else if (playerSelection === "rock" && computerSelection === "rock" || playerSelection === "paper" && computerSelection === "paper" || playerSelection === "scissors" && computerSelection === "scissors") {
-            drawTimes++;
-            return `It's a draw! \n You: ${playerScore} \n Computer: ${computerScore}`
-        }
-        else {
-            return "Invalid play!"
-        }
-    }
+    // function playRound(playerChoice, computerSelection) {
 
-// keep count of the scores
 
-let playerScore = 0;
-let computerScore = 0;
-let drawTimes = 0;
 
 // plays the game until someone gets to 5 points
 
 function game() {
     while (playerScore < 5 && computerScore < 5) {
-        const playerSelection = playerPlay();
+        const playerChoice = playerPlay();
         const computerSelection = computerPlay();
-        alert(playRound(playerSelection, computerSelection))
+        alert(playRound(playerChoice, computerSelection))
     }
 // message at the end of the game, with a count of the score
 alert(`Match is over! The results are:  \n You: ${playerScore}  \n Computer: ${computerScore}  \n Draws: ${drawTimes}`)
 }
+
+function capsFirstLetter(s) {
+    return s.charAt(0).toUpperCase() + s.slice(1);
+  }
+
+function playRound(playerChoice) {
+    const computerSelection = computerChoice();
+    if (playerChoice === "rock" && computerSelection == "scissors" || playerChoice === 'scissors' && computerSelection === "paper" || playerChoice === 'paper' && computerSelection === "rock") {
+        playerScore++;
+        score();
+        resultDisplay.innerHTML = `${capsFirstLetter(playerChoice)} beats ${capsFirstLetter(computerSelection)}! You win!`;
+    }
+    else if (playerChoice === "rock" && computerSelection === "paper" || playerChoice === "paper" && computerSelection === "scissors" || playerChoice === "scissors" && computerSelection === "rock") {
+        computerScore++;
+        score();
+        resultDisplay.innerHTML = `${capsFirstLetter(computerSelection)} beats ${capsFirstLetter(playerChoice)}! You lose!`;
+    }
+    else if (playerChoice === "rock" && computerSelection === "rock" || playerChoice === "paper" && computerSelection === "paper" || playerChoice === "scissors" && computerSelection === "scissors") {
+        score();
+        resultDisplay.innerHTML = "It's a draw!";
+    }
+    else {
+        return "Invalid play!"
+    }
+}
+
+function score() {
+    playerScoreDisplay.innerHTML = playerScore;
+    computerScoreDisplay.innerHTML = computerScore;
+}
+
+function computerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const computerSelection = Math.floor(Math.random() * 3);
+    return choices[computerSelection];
+}
+
+function playerChoices() {
+    rockChoice.addEventListener('click', function () {
+        playRound("rock")
+    })
+
+    paperChoice.addEventListener('click', function () {
+        playRound("paper")
+    })
+
+    scissorsChoice.addEventListener('click', function () {
+        playRound("scissors")
+    })
+}
+
+playerChoices();
+
+resetButton.addEventListener('click', function() {
+    location.reload();
+})
